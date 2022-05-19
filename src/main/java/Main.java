9b011234c6cfa084f551f490873ff2a4754b7379
Stanislav Rakitov <prosperousrf@gmail.com>
2022-05-19 23:54:41 +0300

    Задача 2. Отчет для налоговой

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import com.rakitov.randoms.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Stanislav Rakitov
+ */
+public class Main {
+
+  private static final int MAX_TASKS_AND_THREADS = 3;
+  private static final int MIN_SALES = 10;
+  private static final int MAX_SALES = 1_000;
+  private static final int MIN_PRICE = 10;
+  private static final int MAX_PRICE = 1_000;
+
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+    List<int[]> arrays = new ArrayList<>();
+
+    // Генерация 3 массивов целых положительных чисел
+    for (int i = 0; i < MAX_TASKS_AND_THREADS; i++) {
+      arrays.add(
+          Randoms.getIntArray(
+                  Randoms.getRandomInt(MIN_PRICE, MAX_PRICE), // Минимальное значение цены
+                  Randoms.getRandomInt(MIN_PRICE, MAX_PRICE), // Максимальное значение цены
+                  Randoms.getRandomInt(MIN_SALES, MAX_SALES))); // Количество продаж (он же размер массива)
+    }
+
+    // Атомарная сумма массивов
+    AtomicLong sum = new AtomicLong();
+
+    // Создание трех потоков, которые суммируют выручку (каждый по своему массиву) в общий отчет
+    ExecutorService executorService = Executors.newFixedThreadPool(MAX_TASKS_AND_THREADS);
+    for (int[] array : arrays) {
+      sum.addAndGet(executorService.submit(new Counter(array)).get());
+    }
+    executorService.shutdown();
+    System.out.println("Выручка по всем магазинам: " + sum.get());
+  }
+}
